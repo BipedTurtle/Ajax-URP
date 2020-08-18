7a31a331248a5d721fdb974d4838818c93001a18
James Cho <chojames500@gmail.com>
2020-08-19 01:49:24 +0900

    Indicators no longer jumps from previous location to a new one (IndicatorUtility.cs added)

---
+++ Assets/Scripts/PlayerSystem/IndicatorUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PlayerSystem
+{
+    public enum IndicatorSpawnType
+    {
+        Player,
+        Cursor
+    }
+
+
+    public static class IndicatorUtility
+    {
+        public static Vector3 GetSpawnPos(IndicatorSpawnType indicatorSpawnType)
+        {
+            switch (indicatorSpawnType)
+            {
+                case IndicatorSpawnType.Player:
+                default:
+                    return PlayerController.Instance.transform.localPosition;
+                case IndicatorSpawnType.Cursor:
+                    return ControlUtility.GetMousePositionWorld();
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerSystem/Skill.cs
+++ Assets/Scripts/PlayerSystem/Skill.cs
@@ -6,6 +6,7 @@ namespace PlayerSystem.Skills
     public abstract class Skill : ScriptableObject
     {
         public AssetReferenceGameObject SkillIndicator;
+        public IndicatorSpawnType indicatorSpawnPos;
         private GameObject indicatorGO;
 
         public void DisplayIndicator()
@@ -13,16 +14,20 @@ namespace PlayerSystem.Skills
             if (!SkillIndicator.RuntimeKeyIsValid())
                 return;
 
+            var spawnPos = IndicatorUtility.GetSpawnPos(this.indicatorSpawnPos);
             if (indicatorGO == null)
             {
                 var operation = this.SkillIndicator.InstantiateAsync();
                 operation.Completed += (op) => {
                     this.indicatorGO = op.Result;
+                    this.indicatorGO.transform.localPosition = spawnPos;
                     this.indicatorGO.SetActive(true);
                 };
             }
-            else
+            else {
+                this.indicatorGO.transform.localPosition = spawnPos;
                 this.indicatorGO.SetActive(true);
+            }
         }
 
 
