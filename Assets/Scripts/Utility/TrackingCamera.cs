728f6ffb3584ff8a05d3905d19f28654e2d05180
James Cho <chojames500@gmail.com>
2020-09-01 17:57:12 +0900

    Cameras now track Player

---
+++ Assets/Scripts/Utility/TrackingCamera.cs
@@ -0,0 +1,24 @@
+﻿using Managers;
+using PlayerSystem;
+using UnityEngine;
+
+namespace Utility
+{
+    public class TrackingCamera : MonoBehaviour
+    {
+        private Transform player;
+        private void Start()
+        {
+            this.player = PlayerController.Instance.transform;
+
+            UpdateManager.Instance.SubscribeToGlobalUpdate(this.TrackPlayer);
+        }
+
+
+        private void TrackPlayer()
+        {
+            float cameraY = transform.localPosition.y;
+            transform.localPosition = this.player.localPosition.Set(y: cameraY);
+        }
+    }
+}
